88926733773cbc3c62fc783fa46bdd8839c6484c
Jessica Spivak <jls103193@yahoo.com>
2023-03-23 12:25:33 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+    Pseudocode:
+    1. create/declare three variables.
+    2. assign values using math to equal the numbers in the combination. 
+    3. output the combination using alert/console.log
+*/
+let firstNumber = 5 * 2;
+let secondNumber = 53 - 13;
+let thirdNumber = 30 + 9;
+// alert(firstNumber + " " + secondNumber + " " + thirdNumber);
+console.log(firstNumber + " " + secondNumber + " " + thirdNumber);
